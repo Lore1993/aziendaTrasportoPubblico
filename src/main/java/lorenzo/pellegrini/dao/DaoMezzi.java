785d7d795ad7f3714732bfc66cdbfc1bfd403774
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-01-26 16:12:53 +0100

    dao_packages

---
+++ src/main/java/lorenzo/pellegrini/dao/DaoMezzi.java
@@ -0,0 +1,21 @@
+package lorenzo.pellegrini.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import lorenzo.pellegrini.entities.Mezzo;
+
+public class DaoMezzi {
+	private EntityManager em;
+
+	public DaoMezzi(EntityManager em) {
+		this.em = em;
+	}
+
+	public void salvaMezzi(Mezzo mezzo) {
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		em.persist(mezzo);
+		tx.commit();
+		System.out.println("il mezzo "+mezzo+" è stato ");
+	}
+}
