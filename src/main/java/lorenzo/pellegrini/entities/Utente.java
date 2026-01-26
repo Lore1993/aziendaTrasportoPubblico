e339ca1f112d6a1964d06bc092c5b08c51d0ab02
alexa109445 <alemanolache109@gmail.com>
2026-01-26 15:45:47 +0100

    sistemo errore

--- src/main/java/lorenzo/pellegrini/entities/Utente.java
+++ src/main/java/lorenzo/pellegrini/entities/Utente.java
@@ -27,14 +27,14 @@ public class Utente {
         this.cognome=cognome;
         this.email=email;
     }
-
-    public Long getId(){return id;}
-    public String getNome (){return nome;}
-    public void setNome(String nome){this.nome=nome};
-    public String getCognome(String cognome){this.cognome=cognome;}
-    public String getEmail(){return email;}
-    public void setEmail(String email){this.email=email;}
-    public Tessera getTessera(){return tessera;}
-    public void setTessera(Tessera tessera){this.tessera=tessera;}
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCognome() { return cognome; }
+    public void setCognome(String cognome) { this.cognome = cognome; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public Tessera getTessera() { return tessera; }
+    public void setTessera(Tessera tessera) { this.tessera = tessera; }
 
 }
