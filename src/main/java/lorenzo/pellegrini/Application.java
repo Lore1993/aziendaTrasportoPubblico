f0fdc9e114b7a51ebef78e3ad733cab33428d30a
alexa109445 <alemanolache109@gmail.com>
2026-01-26 12:18:21 +0100

    collegamento database

--- src/main/java/lorenzo/pellegrini/Application.java
+++ src/main/java/lorenzo/pellegrini/Application.java
@@ -1,8 +1,23 @@
 package lorenzo.pellegrini;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("buildweek1db");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Collegameto database riuscito");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
