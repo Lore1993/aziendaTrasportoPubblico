6f9ac10a0daa0fd841454f7c7db8d23415c3747f
alexa109445 <alemanolache109@gmail.com>
2026-01-27 15:02:43 +0100

    Update PuntoVenditaDAO.java

--- src/main/java/lorenzo/pellegrini/dao/PuntoVenditaDAO.java
+++ src/main/java/lorenzo/pellegrini/dao/PuntoVenditaDAO.java
@@ -2,13 +2,9 @@ package lorenzo.pellegrini.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.TypedQuery;
-import lorenzo.pellegrini.entities.DistributoreAutomatico;
+import lorenzo.pellegrini.entities.Biglietto;
 import lorenzo.pellegrini.entities.PuntoVendita;
-import lorenzo.pellegrini.entities.TitoloViaggio;
-
 import java.time.LocalDate;
-import java.util.List;
 
 public class PuntoVenditaDAO {
     private EntityManager em;
@@ -32,44 +28,19 @@ public class PuntoVenditaDAO {
         return trovato;
     }
 
-    // imposto un nuovo stato di un distributore (se attivo o meno)
-    public void impostaStatoDistributore(Long id, boolean nuovoStato) {
+    // Metodo per vendere un biglietto
+    public void vendiBiglietto(PuntoVendita pv) {
+        // Creo il biglietto
+        Biglietto b = new Biglietto();
+        b.setDataEmissione(LocalDate.now());
+        b.setPuntoVendita(pv);
+
+        // Salvo il biglietto
         EntityTransaction t = em.getTransaction();
-        try {
-            t.begin();
-            PuntoVendita pv = em.find(PuntoVendita.class, id);
+        t.begin();
+        em.persist(b);
+        t.commit();
 
-            if (pv instanceof DistributoreAutomatico) {
-                DistributoreAutomatico dist = (DistributoreAutomatico) pv;
-                dist.setAttivo(nuovoStato);
-
-                System.out.println("Il distributore " + dist.getNome() + " è ora " + (nuovoStato ? "ATTIVO" : "FUORI SERVIZIO"));
-            }
-        } catch (Exception e) {
-            if (t.isActive()) t.rollback();
-            System.err.println("Errore: " + e.getMessage());
-        }
-    }
-
-    // cerco solo i distributori automatici attivi
-    public List<DistributoreAutomatico> findDistributoriAttivi() {
-        TypedQuery<DistributoreAutomatico> query = em.createQuery(
-                "SELECT d FROM DistributoreAutomatico d WHERE d.attivo = true",
-                DistributoreAutomatico.class
-        );
-        return query.getResultList();
-    }
-
-    // conto quanti titoli ha emesso un determinato punto Vendita in un lasso di tempo
-    public Long countTitoliEmessi(Long puntoVenditaId, LocalDate inizio, LocalDate fine) {
-        TypedQuery<Long> query = em.createQuery("SELECT COUNT(t) FROM TitoloViaggio t " +
-                "WHERE t.puntoVendita.id = :id " +
-                "AND t.dataEmissione BETWEEN :inizio AND :fine ", Long.class);
-
-        query.setParameter("id", puntoVenditaId);
-        query.setParameter("inizio", inizio);
-        query.setParameter("fine", fine);
-
-        return query.getSingleResult();
+        System.out.println("Biglietto venduto da " + pv.getNome());
     }
 }
