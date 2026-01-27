71a09f96498149c9caeb940ecd70d03b8d78c924
Lore1993 <lorenzo93pellegrini@gmail.com>
2026-01-27 12:26:33 +0100

    h
    h

--- src/main/java/lorenzo/pellegrini/entities/Mezzo.java
+++ src/main/java/lorenzo/pellegrini/entities/Mezzo.java
@@ -1,91 +1,54 @@
 package lorenzo.pellegrini.entities;
 
 import jakarta.persistence.*;
+import lorenzo.pellegrini.enums.StatoAttuale;
 import lorenzo.pellegrini.enums.TipoMezzo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Table(name = "mezzo")
-public class Mezzo {
+@Table(name = "Mezzo")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_mezzo", discriminatorType = DiscriminatorType.STRING)
+public abstract class Mezzo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Cambiato da UUID a IDENTITY
     private Long id;
 
-    @Column(name = "capacita", nullable = false)
-    private int capacita;
-
     @Enumerated(EnumType.STRING)
+    @Column(name = "stato_attuale", nullable = false)
+    private StatoAttuale statoAttuale;
+    @Enumerated(EnumType.STRING)
+
     @Column(name = "tipo_mezzo", nullable = false)
     private TipoMezzo tipoMezzo;
 
-    @OneToMany(mappedBy = "mezzo")
-    private List<Percorrenza> percorrenze = new ArrayList<>();
+    @Column(name = "capacita")
+    private int capacita;
 
-    @OneToMany(mappedBy = "mezzo")
-    private List<StatoMezzo> storicoStati = new ArrayList<>();
-
-    // Costruttori
     public Mezzo() {
     }
 
-    public Mezzo(int capacita, TipoMezzo tipoMezzo) {
+    public Mezzo(StatoAttuale statoAttuale, int capacita) {
+        this.statoAttuale = statoAttuale;
         this.capacita = capacita;
-        this.tipoMezzo = tipoMezzo;
     }
 
-    // Getter e Setter
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
-    public int getCapacita() {
-        return capacita;
+    public StatoAttuale getStatoAttuale() {
+        return statoAttuale;
     }
 
-    public void setCapacita(int capacita) {
-        this.capacita = capacita;
-    }
-
-    public TipoMezzo getTipoMezzo() {
-        return tipoMezzo;
-    }
-
-    public void setTipoMezzo(TipoMezzo tipoMezzo) {
-        this.tipoMezzo = tipoMezzo;
-    }
-
-    public List<Percorrenza> getPercorrenze() {
-        return percorrenze;
-    }
-
-    public void setPercorrenze(List<Percorrenza> percorrenze) {
-        this.percorrenze = percorrenze;
-    }
-
-    public List<StatoMezzo> getStoricoStati() {
-        return storicoStati;
-    }
-
-    public void setStoricoStati(List<StatoMezzo> storicoStati) {
-        this.storicoStati = storicoStati;
-    }
-
-    // Metodo helper per ottenere lo stato attuale
-    public StatoMezzo getStatoAttuale() {
-        return storicoStati.stream()
-                .filter(s -> s.getDataFine() == null)
-                .findFirst()
-                .orElse(null);
+    public void setStatoAttuale(StatoAttuale statoAttuale) {
+        this.statoAttuale = statoAttuale;
     }
 
     @Override
     public String toString() {
         return "Mezzo{" +
                 "id=" + id +
-                ", tipo=" + tipoMezzo +
-                ", capacita=" + capacita +
+                ", statoAttuale=" + statoAttuale +
                 '}';
     }
 }
